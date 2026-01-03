1a269baf1a15147a40d21c1552ae738dde812d1d
barkin <barkinsagin@gmail.com>
2026-01-03 17:13:18 +0300

    feat: smoothen scroll display with more fade in animation usage

--- app/_components/TechStack/index.tsx
+++ app/_components/TechStack/index.tsx
@@ -45,24 +45,27 @@ const skills = {
 
 const TechStack = () => {
   return (
-    <WithFadeInAnimation>
-      <section id="tech-stack" className="py-20 bg-slate-50/50 dark:bg-slate-900/30">
-      <h2 className="text-3xl font-bold text-center mb-4">My Tech Stack</h2>
-      <p className="text-xl text-center text-slate-600 dark:text-slate-300 mb-12">Capabilities I bring to product building</p>
+    <section id="tech-stack" className="py-20 bg-slate-50/50 dark:bg-slate-900/30">
+      <WithFadeInAnimation>
+        <h2 className="text-3xl font-bold text-center mb-4">My Tech Stack</h2>
+        <p className="text-xl text-center text-slate-600 dark:text-slate-300 mb-12">Technologies and tools I use to ship products</p>
+      </WithFadeInAnimation>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-8 mx-10 sm:mx-40 md:mx-60">
         {Object.entries(skills).map(([category, techList]) => (
-          <Card key={category}>
-            <h3 className="text-xl font-bold mb-4">{category}</h3>
-            <div className='flex flex-wrap'>
-            {techList.map((tech) => (
-                <span key={tech} className='rounded-full bg-slate-200/50 dark:bg-slate-800/50 mr-2 mb-3 px-3 py-1'>{tech}</span>
-              ))}
-            </div>
-          </Card>
+          <WithFadeInAnimation key={category}>
+            <Card>
+              <h3 className="text-xl font-bold mb-4">{category}</h3>
+              <div className='flex flex-wrap'>
+                {techList.map((tech) => (
+                  <span key={tech} className='rounded-full bg-slate-200/50 dark:bg-slate-800/50 mr-2 mb-3 px-3 py-1'>{tech}</span>
+                ))}
+              </div>
+            </Card>
+          </WithFadeInAnimation>
+
         ))}
       </div>
-      </section>
-    </WithFadeInAnimation>
+    </section>
   );
 };
 
