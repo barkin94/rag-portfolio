387328fc3d4378351d72661e1cbb46ccfc6a7a22
barkin <barkinsagin@gmail.com>
2026-01-05 15:09:06 +0300

    refactor: reduce divs in header

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -6,24 +6,20 @@ export default async function Header() {
   const isDark = (await cookies()).get('isDark')?.value === '1';
 
   return (
-    <header className="w-full border-b border-slate-200 dark:border-slate-800 sticky top-0 z-50 shadow-sm backdrop-blur">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          {/* Left Division - Name */}
-          <div className="flex items-center justify-center flex-1">
-            <h4 className="text-xl font-bold text-slate-900 dark:text-slate-100">
-              Barkin Buyuksagin
-            </h4>
-          </div>
+    <header className="border-b border-slate-200 dark:border-slate-800 sticky top-0 z-50 shadow-sm backdrop-blur container mx-auto px-4 flex items-center justify-between h-16">
+      {/* Left Division - Name */}
+      <div className="flex items-center justify-center flex-1">
+        <h4 className="text-xl font-bold text-slate-900 dark:text-slate-100">
+          Barkin Buyuksagin
+        </h4>
+      </div>
 
-          {/* Middle Division - Navigation Links */}        
-          <NavLinks />
+      {/* Middle Division - Navigation Links */}
+      <NavLinks />
 
-          {/* Right Division - Theme Toggle */}
-          <div className="flex items-center justify-center flex-1">
-            <ThemeToggleButton isDark={isDark} />
-          </div>
-        </div>
+      {/* Right Division - Theme Toggle */}
+      <div className="flex items-center justify-center flex-1">
+        <ThemeToggleButton isDark={isDark} />
       </div>
     </header>
   );
