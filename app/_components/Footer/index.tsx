a737a9ef9b5879e695e40091620a77fe5400aaae
barkin <barkinsagin@gmail.com>
2026-01-05 15:05:23 +0300

    feat: add footer

---
+++ app/_components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="border-t border-slate-200 dark:border-slate-800 shadow-sm text-center text-slate-400 text-sm pt-6 pb-20">
+      {`© ${currentYear} Barkin Buyuksagin. Powered by Next.js, Tailwind CSS, custom RAG and lots of coffee. ☕`}
+    </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Header from "./_components/Header";
 import TechStack from "./_components/TechStack";
 import Home from "./_components/Home";
 import ParticlesBackground from "./_components/ParticlesBackground";
+import Footer from "./_components/Footer";
 
 export default async function Main() { 
   return (
@@ -16,6 +17,7 @@ export default async function Main() {
         <Journey />
         <Chat />
       </main>
+      <Footer />
     </div>
   )
 }
