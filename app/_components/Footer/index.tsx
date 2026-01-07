038e6d206b498f4896f63dd1d2185a42c179401e
barkin <barkinsagin@gmail.com>
2026-01-07 15:28:34 +0300

    chore: polish footer style

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="border-t border-slate-200 dark:border-slate-800 shadow-sm text-center text-slate-400 text-sm pt-6 pb-20">
+    <footer className="border-t border-slate-200 dark:border-slate-800 shadow-sm text-center text-slate-500 text-sm pt-6 pb-12">
       {`© ${currentYear} Barkin Buyuksagin. Powered by Next.js, Tailwind CSS, custom RAG and lots of coffee. ☕`}
     </footer>
   );
